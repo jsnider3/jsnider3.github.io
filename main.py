a1f891ceb1047acc84f947e2eea8a3f5d4e4f121
Joshua Snider <josh@joshuasnider.com>
2016-02-26 23:39:50 -0500

    Fixes 404 not working and QuisCustodiet.

--- main.py
+++ main.py
@@ -6,8 +6,7 @@
 from flask import Flask
 from flask import render_template
 import os
-app = Flask(__name__,
-  static_url_path=os.path.join(os.path.dirname(__file__),'/static/'))
+app = Flask(__name__)
 
 @app.route('/api/tagline/')
 def get_tagline():
@@ -16,10 +15,11 @@ def get_tagline():
 @app.errorhandler(404)
 def page_not_found(e):
   """Custom 404 error."""
+  print("HELLO from 404")
   return render_template('404.html')
 
 @app.errorhandler(500)
 def application_error(e):
-  """TODO custom 500 error."""
+  """Custom 500 error."""
   return 'Sorry, unexpected error: {}'.format(e), 500
 
