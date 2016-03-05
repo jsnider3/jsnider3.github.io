8dd2fbdd0fd5335284fa58cd75e7aa025bc13112
Joshua Snider <josh@joshuasnider.com>
2016-03-05 01:51:16 -0500

    One more challenge.

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ def get_tagline():
 def letsencrypt_challenge():
   return "CLubHhYvkZj2ndjHNsETwDNvj6KriherBtAPhDNUTqw.RJszauNUxRavajsIJkOIl7hqRX-t5gk6J0vd7-RyIf8"
 
+@app.route('/.well-known/acme-challenge/gqfq8DC9iaLftcGGyMr4k0xExSWMmZKs5Gyg3wPm0HA')
+def letsencrypt_challenge2():
+  return "gqfq8DC9iaLftcGGyMr4k0xExSWMmZKs5Gyg3wPm0HA.RJszauNUxRavajsIJkOIl7hqRX-t5gk6J0vd7-RyIf8"
+
 @app.errorhandler(404)
 def page_not_found(e):
   """Custom 404 error."""
