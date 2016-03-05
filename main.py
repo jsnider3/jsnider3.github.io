2dba18e9bb816dfe6d4f63caeddfe205b2054795
Joshua Snider <josh@joshuasnider.com>
2016-03-05 03:40:35 -0500

    Refactoring lets-encrypt.

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ def get_tagline():
   ind = hash(request.args.get('page', '')) - hash('')
   return taglines[ind % len(taglines)]
 
-@app.route('/.well-known/acme-challenge/CLubHhYvkZj2ndjHNsETwDNvj6KriherBtAPhDNUTqw')
-def letsencrypt_challenge():
-  return "CLubHhYvkZj2ndjHNsETwDNvj6KriherBtAPhDNUTqw.RJszauNUxRavajsIJkOIl7hqRX-t5gk6J0vd7-RyIf8"
-
-@app.route('/.well-known/acme-challenge/gqfq8DC9iaLftcGGyMr4k0xExSWMmZKs5Gyg3wPm0HA')
-def letsencrypt_challenge2():
-  return "gqfq8DC9iaLftcGGyMr4k0xExSWMmZKs5Gyg3wPm0HA.RJszauNUxRavajsIJkOIl7hqRX-t5gk6J0vd7-RyIf8"
+@app.route('/.well-known/acme-challenge/<challenge>')
+def letsencrypt_challenge(challenge):
+  challenges = {'CLubHhYvkZj2ndjHNsETwDNvj6KriherBtAPhDNUTqw':
+                "CLubHhYvkZj2ndjHNsETwDNvj6KriherBtAPhDNUTqw.RJszauNUxRavajsIJkOIl7hqRX-t5gk6J0vd7-RyIf8",
+                'gqfq8DC9iaLftcGGyMr4k0xExSWMmZKs5Gyg3wPm0HA' :
+                "gqfq8DC9iaLftcGGyMr4k0xExSWMmZKs5Gyg3wPm0HA.RJszauNUxRavajsIJkOIl7hqRX-t5gk6J0vd7-RyIf8"}
+  return challenges[challenge]
 
 @app.errorhandler(404)
 def page_not_found(e):
